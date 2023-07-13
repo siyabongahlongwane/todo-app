d233d47407851adc03a035307f20a284ddea0831
Siyabonga <hlongwanesiyabonga6@gmail.com>
2023-07-13 22:38:45 +0200

    Set up basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContainerComponent } from './container/container.component';
+import { TaskviewComponent } from './taskview/taskview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'app/task-view'
+  },
+  {
+    path: 'app', component: ContainerComponent, children: [
+      {
+        path: '', pathMatch: 'full', redirectTo: 'task-view'
+      },
+      {
+        path: 'task-view', component: TaskviewComponent
+      },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
